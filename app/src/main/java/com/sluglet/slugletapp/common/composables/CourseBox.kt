bacedd5b8e99c5c21a86a214176fae5cbe7641f7
Camden Beard <cfbeard@ucsc.edu>
2023-10-21 17:05:37 -0700

    file structure changes, starting screens and composables

---
+++ app/src/main/java/com/sluglet/slugletapp/common/composables/CourseBox.kt
@@ -0,0 +1,37 @@
+package com.sluglet.slugletapp.common.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.absoluteOffset
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CourseBox(
+    Course: String,
+    Prof: String,
+    DateTime: String,
+    
+    modifier: Modifier
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(color = Color.White, shape = RoundedCornerShape(size = 20.dp))
+    ) {
+
+    }
+}
+
+@Preview (showBackground = true)
+@Composable
+fun CourseBoxPreview (
+) {
+
+}
