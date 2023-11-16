36fccf79ba469bded58e83ce0048b81a6d2178ee
Camden Beard <cfbeard@ucsc.edu>
2023-11-16 13:54:14 -0800

    Notes to self.

--- app/src/main/java/com/sluglet/slugletapp/screens/map/MapViewModel.kt
+++ app/src/main/java/com/sluglet/slugletapp/screens/map/MapViewModel.kt
@@ -15,16 +15,18 @@ class MapViewModel @Inject constructor(
     logService: LogService,
 
 ) : SlugletViewModel(logService) {
+    // FIXME: Would be ideal to keep the state of the map
+    //        so that when navigating away and coming back
+    //        it doesn't revert to this starting state
     private var _cameraState = mutableStateOf(
         CameraPositionState(
             CameraProperty(
                 // 36°59'44.5"N 122°03'35.5"W
+                // Starting point and zoom for the map
                 geoPoint = GeoPoint(36.99582810669116, -122.05824150361903),
                 zoom = 15.0
             )
         )
     )
     val cameraState: State<CameraPositionState> = _cameraState
-
-
 }
