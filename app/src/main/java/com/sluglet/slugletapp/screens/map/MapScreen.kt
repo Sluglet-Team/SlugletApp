8604ad4837bda25d6df8898c1445bff1df7596ad
Camden Beard <cfbeard@ucsc.edu>
2023-11-20 21:37:21 -0800

    Task A6 done. User courses are now displayed as markers on the map.  Changes to firestore are reflected in real time.  If a user adds a course, the map recomposes with a new marker.  Once users are able to delete courses, this should also be reflected.  Right now that functionality has been tested by manually deleting courses from the firestore console, which works as expected.

--- app/src/main/java/com/sluglet/slugletapp/screens/map/MapScreen.kt
+++ app/src/main/java/com/sluglet/slugletapp/screens/map/MapScreen.kt
@@ -15,11 +15,7 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.sluglet.slugletapp.OSMaps.OSMaps
 import com.sluglet.slugletapp.R
 import com.sluglet.slugletapp.common.composables.CourseMarker
-import com.sluglet.slugletapp.model.CourseData
 import com.sluglet.slugletapp.resources
-import com.sluglet.slugletapp.screens.search.SearchViewModel
-import kotlinx.coroutines.flow.emptyFlow
-import org.osmdroid.util.GeoPoint
 
 
 @Composable
@@ -29,36 +25,32 @@ fun MapScreen (
 ) {
     // Gets state from the view model for the camera position
     val cameraPositionState = viewModel.cameraState
+    // Collect as state with lifecycle means that anytime the state of the flow changes
+    // the value will change and cause recomposition
+    val userCourses by viewModel.userCourses.collectAsStateWithLifecycle(emptyList())
     val courseToDisplay = viewModel.courseToDisplay.collectAsStateWithLifecycle(null).value
-    val markerList = viewModel.markerList.collectAsStateWithLifecycle(emptyList()).value
 
     // Use this modifier to modify the look of the map
     val mapModifier = Modifier
         .padding(start = 10.dp, top = 10.dp, end = 10.dp)
         .clip(RoundedCornerShape(10.dp))
         .shadow(elevation = 10.dp)
-    // FIXME: Remove
-    val test = CourseData (
-        course_number = "CSE 115A",
-        course_name = "Intro to Software Engineering",
-        location = "Baskin Auditorium 1",
-        date_time = "MWF 8:00am-9:00am",
-        prof_name = "Julig",
-        latitude = 36.99924676842543,
-        longitude = -122.060734332498
-    )
-    viewModel.addMarker(test)
-    if (courseToDisplay != null) {
-        viewModel.addMarker(courseToDisplay)
-    }
+
     OSMaps (
         cameraPositionState = cameraPositionState,
         modifier = mapModifier
     ) {
-        markerList.forEach { course->
+        userCourses.forEach { course->
             CourseMarker(
                 course = course,
-                markerIcon = ResourcesCompat.getDrawable(resources(), R.drawable.edu_map_pin, null)
+                markerIcon = ResourcesCompat.getDrawable(resources(), R.drawable.edu_map_pin_red, null)
+            )
+        }
+        // Course to display is null until map button clicked on search screen
+        if (courseToDisplay != null) {
+            CourseMarker(
+                course = courseToDisplay,
+                markerIcon = ResourcesCompat.getDrawable(resources(), R.drawable.edu_map_pin_green, null)
             )
         }
     }
--- app/src/main/java/com/sluglet/slugletapp/screens/map/MapViewModel.kt
+++ app/src/main/java/com/sluglet/slugletapp/screens/map/MapViewModel.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.SavedStateHandleSaveableApi
 import androidx.lifecycle.viewmodel.compose.saveable
 import com.sluglet.slugletapp.OSMaps.CameraPositionState
@@ -12,20 +13,22 @@ import com.sluglet.slugletapp.OSMaps.CameraProperty
 import com.sluglet.slugletapp.model.CourseData
 import com.sluglet.slugletapp.model.service.LogService
 import com.sluglet.slugletapp.model.service.MapService
+import com.sluglet.slugletapp.model.service.StorageService
 import com.sluglet.slugletapp.screens.SlugletViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import org.osmdroid.util.GeoPoint
 import javax.inject.Inject
-
+import javax.inject.Singleton
 @HiltViewModel
 class MapViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
     logService: LogService,
-    private val mapService: MapService
+    private val mapService: MapService,
+    private val storageService: StorageService
 ) : SlugletViewModel(logService) {
-    // FIXME: Would be ideal to keep the state of the map
+    // FIXME(SPRINT 4): Would be ideal to keep the state of the map
     //        so that when navigating away and coming back
     //        it doesn't revert to this starting state
     //        Below is an attempt at doing this but is maybe not implemented right
@@ -42,18 +45,18 @@ class MapViewModel @Inject constructor(
             )
         )
     }
+    // FIXME(REMOVE): Previous impl used a more standard state handling mechanism
+    //        See previous commits.
     // val cameraState: State<CameraPositionState> = _cameraState
+
+    // Get the course to display from the map service
+    // This only has a value if the map button on the search screen is clicked
     var courseToDisplay = mapService.courseToDisplay
-    private val _markerList : MutableStateFlow<MutableList<CourseData>> = MutableStateFlow(
-        mutableListOf()
-    )
-    val markerList = _markerList.asStateFlow()
+    // Get user courses from firestore
+    val userCourses = storageService.userCourses
 
-    fun addMarker(course : CourseData) {
-        _markerList.value.add(course)
-    }
-
-    fun removeMarker(course : CourseData) {
-        _markerList.value.remove(course)
-    }
+    // NOTE: removed markerlist because it isn't needed
+    // userCourses can be used, and is automatically reflected
+    // by changes in firestore so removing a marker would be when
+    // a user removes a course.
 }
