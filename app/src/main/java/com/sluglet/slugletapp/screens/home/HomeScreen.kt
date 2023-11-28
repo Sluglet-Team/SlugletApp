29f0320343de29420424f2fcd16e3d0ec6e77ef5
Camden Beard <cfbeard@ucsc.edu>
2023-11-28 09:06:01 -0800

    Small fix.  Should be ready for merge to main.

--- app/src/main/java/com/sluglet/slugletapp/screens/home/HomeScreen.kt
+++ app/src/main/java/com/sluglet/slugletapp/screens/home/HomeScreen.kt
@@ -107,7 +107,7 @@ fun HomeScreenContent(
                 verticalArrangement = Arrangement.spacedBy(15.dp)
             ) {
                 items(courses) {courseItem ->
-                    CourseBox(coursedata = courseItem, onAddClick = null)
+                    CourseBox(coursedata = courseItem, onAddClick = null, onMapClick = null)
                 }
             }
 
